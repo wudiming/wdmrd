9301917c50f5a12d9d2fc2d13afd8c3b9f783791
wudiming <sxwudiming@163.com>
2025-01-25 06:51:59 +0800

    25

--- src/common.rs
+++ src/common.rs
@@ -910,7 +910,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://rdapi.881128.xyz".to_owned()
+    "https://rd.1128.pp.ua".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
